8716e7a6c699fa3255537a7f0a7a4c67390b3742
Robson Rodrigues Ribeiro <projetos.r@hotmail.com>
2022-12-22 00:17:10 -0300

    ajustado timeline

--- src/components/TimeLineExperience/index.tsx
+++ src/components/TimeLineExperience/index.tsx
@@ -15,13 +15,6 @@ import './Timeline.css'
 
 const TimeLineExperience = ({timeline}: IExperienceProps) => {
 
-    const [event1, setEvent1] = useState([
-        { jobTitle: 'Ordered', date: '15/10/2020 10:30', icon: 'pi pi-shopping-cart', color: '#9C27B0', image: 'game-controller.jpg', description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deseruntquisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas!" },
-        { jobTitle: 'Processing', date: '15/10/2020 14:00', icon: 'pi pi-cog', color: '#673AB7', description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deseruntquisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas!" },
-        { jobTitle: 'Shipped', date: '15/10/2020 16:15', icon: 'pi pi-shopping-cart', color: '#FF9800', description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deseruntquisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas!" },
-        { jobTitle: 'Delivered', date: '16/10/2020 10:00', icon: 'pi pi-check', color: '#607D8B', description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deseruntquisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas!" }
-    ])
-
     const customizedMarker = (item: ExperienceProps) => {
         return (
             <span className="custom-marker shadow-1" style={{ backgroundColor: item.color }}>
@@ -32,8 +25,10 @@ const TimeLineExperience = ({timeline}: IExperienceProps) => {
 
     const customizedContent = (item: ExperienceProps) => {
         return (
-            <Card title={item.jobTitle} subTitle={item.date} header={item.company}>
+            <Card header={item.company}>
                 { item.image && <img src={item.image} onError={(e) => <img src='https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png' />} alt={item.name} width={200} className="shadow-1" />}
+                <div><span >{item.date}</span></div>
+                <div><span style={{fontWeight: "bold"}}>{item.jobTitle}</span></div>
                 <p>{item.description}</p>
             </Card>
         );
